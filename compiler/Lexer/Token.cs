71b62547734cae1b1950f724f1d9dab780efa62f
Gabriel Bonilla Pérez <146474128+gabrielbnp@users.noreply.github.com>
2024-07-15 06:05:52 -0400

    feature: adds a class to represent the tokens
    A token stores information about its type, lexeme, and value, as well as its position in the source code.

---
+++ compiler/Lexer/Token.cs
@@ -0,0 +1,45 @@
+public enum TokenType
+{
+    // single-character tokens
+    LEFT_PAR, RIGHT_PAR,
+    PLUS, MINUS, STAR, SLASH,
+
+    // one or two character tokens
+    EQUAL, EQUAL_EQUAL,
+    BANG, BANG_EQUAL,
+    LESS, LESS_EQUAL,
+    GREATER, GREATER_EQUAL,
+
+    // literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // keywords
+    AND, OR, TRUE, FALSE
+}
+
+public class Token
+{
+    public TokenType type;
+    public string? lexeme;
+    public object? literal;
+    public int numLine;
+    public int numColumn;
+
+    public Token(TokenType type, string? lexeme, object? literal, int numLine, int numColumn)
+    {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+
+        this.numLine = numLine;
+        this.numColumn = numColumn;
+    }
+
+    public string toString()
+    {
+        if(literal == null)
+            return "[" + type + "] \'" + lexeme + "\'";
+
+        return "[" + type + "] \'" + lexeme + "\' " + (string) literal;  
+    }
+}
